00f3ea93863f5f5cbcd8205dd1e2e24e24dc3142
Olivier Smet <o.smet@live.nl>
2023-08-26 16:22:12 +0200

    Add NVNS Q1 non-sorting benchmark

---
+++ src/main/java/benchmarks/tpch/q1_no_sort_hard_coded/NonVectorisedNonSimd.java
@@ -0,0 +1,260 @@
+package benchmarks.tpch.q1_no_sort_hard_coded;
+
+import evaluation.codegen.infrastructure.data.ArrowTableReader;
+import evaluation.codegen.infrastructure.data.CachingArrowTableReader;
+import evaluation.general_support.hashmaps.Char_Arr_Hash_Function;
+import org.apache.arrow.memory.RootAllocator;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This benchmark evaluates the performance of AethraDB using its non-vectorised query code
+ * generation without SIMD-ed operators, but while not actually invoking the code generator itself.
+ */
+@State(Scope.Benchmark)
+public class NonVectorisedNonSimd {
+
+    /**
+     * Different instances of the TPC-H database can be tested using this benchmark.
+     */
+    @Param({
+        "/nvtmp/AethraTestData/tpch/sf-1"
+    })
+    private String tpchInstance;
+
+    /**
+     * State: the {@link RootAllocator} used for reading Arrow files.
+     */
+    private RootAllocator rootAllocator;
+
+    /**
+     * State: the {@link ArrowTableReader} used for reading the lineitem table.
+     */
+    private ArrowTableReader lineitem_table;
+
+    /**
+     * State: the hash-table which is used by the query to aggregate.
+     */
+    private AggregationMap aggregation_state_map;
+
+    /**
+     * State: the result of the query.
+     */
+    private byte[][] resultReturnFlag;
+    private byte[][] resultLineStatus;
+    private double[] resultSumQuantity;
+    private double[] resultSumBasePrice;
+    private double[] resultSumDiscPrice;
+    private double[] resultSumCharge;
+    private double[] resultAvgQuantity;
+    private double[] resultAvgPrice;
+    private double[] resultAvgDisc;
+    private int[] resultCountOrder;
+
+    /**
+     * State: the result verifier for the query.
+     */
+    private ResultVerifier resultVerifier;
+
+    /**
+     * Method to set up teh state at the start of each benchmark fork.
+     */
+    @Setup(Level.Trial)
+    public void trialSetup() throws Exception {
+        // Setup the database
+        this.rootAllocator = new RootAllocator();
+        this.lineitem_table = new CachingArrowTableReader(
+                new File(this.tpchInstance + "/lineitem.arrow"), this.rootAllocator);
+
+        // Initialise the hash-table
+        this.aggregation_state_map = new AggregationMap();
+
+        // Initialise the result
+        int resultSize = 4;
+        this.resultReturnFlag = new byte[resultSize][];
+        Arrays.fill(this.resultReturnFlag, null);
+        this.resultLineStatus = new byte[resultSize][];
+        Arrays.fill(this.resultLineStatus, null);
+        this.resultSumQuantity = new double[resultSize];
+        Arrays.fill(this.resultSumQuantity, -1);
+        this.resultSumBasePrice = new double[resultSize];
+        Arrays.fill(this.resultSumBasePrice, -1);
+        this.resultSumDiscPrice = new double[resultSize];
+        Arrays.fill(this.resultSumDiscPrice, -1);
+        this.resultSumCharge = new double[resultSize];
+        Arrays.fill(this.resultSumCharge, -1);
+        this.resultAvgQuantity = new double[resultSize];
+        Arrays.fill(this.resultAvgQuantity, -1);
+        this.resultAvgPrice = new double[resultSize];
+        Arrays.fill(this.resultAvgPrice, -1);
+        this.resultAvgDisc = new double[resultSize];
+        Arrays.fill(this.resultAvgDisc, -1);
+        this.resultCountOrder = new int[resultSize];
+        Arrays.fill(this.resultCountOrder, -1);
+
+        // Initialise the result verifier
+        this.resultVerifier = new ResultVerifier(this.tpchInstance + "/q1_result.csv");
+    }
+
+    /**
+     * This method sets up the state at the start of each benchmark iteration.
+     */
+    @Setup(Level.Invocation)
+    public void invocationStetup() throws Exception {
+        // Reset the table
+        this.lineitem_table.reset();
+
+        // Reset the aggregation map
+        this.aggregation_state_map.reset();
+    }
+
+    /**
+     * This method verifies successful completion of the previous benchmark and cleans up after it.
+     */
+    @TearDown(Level.Invocation)
+    public void teardown() {
+        // Verify the result
+        if (!this.resultVerifier.resultCorrect(
+                this.resultReturnFlag,
+                this.resultLineStatus,
+                this.resultSumQuantity,
+                this.resultSumBasePrice,
+                this.resultSumDiscPrice,
+                this.resultSumCharge,
+                this.resultAvgQuantity,
+                this.resultAvgPrice,
+                this.resultAvgDisc,
+                this.resultCountOrder)
+        ) {
+            throw new RuntimeException("The computed result is incorrect");
+        }
+
+        // Reset the result after verifying it
+        Arrays.fill(this.resultReturnFlag, null);
+        Arrays.fill(this.resultLineStatus, null);
+        Arrays.fill(this.resultSumQuantity, -1);
+        Arrays.fill(this.resultSumBasePrice, -1);
+        Arrays.fill(this.resultSumDiscPrice, -1);
+        Arrays.fill(this.resultSumCharge, -1);
+        Arrays.fill(this.resultAvgQuantity, -1);
+        Arrays.fill(this.resultAvgPrice, -1);
+        Arrays.fill(this.resultAvgDisc, -1);
+        Arrays.fill(this.resultCountOrder, -1);
+    }
+
+    /**
+     * This method performs the actual execution of the query for benchmarking.
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Fork(jvmArgsAppend = {
+            "--add-modules=jdk.incubator.vector",
+            "--add-opens=java.base/java.nio=ALL-UNNAMED",
+            "-Darrow.enable_unsafe_memory_access=true",
+            "-Darrow.enable_null_check_for_get=false",
+            "--enable-preview",
+            "-Xmx16g",
+            "-Xms8g"
+    })
+    public void executeQuery(Blackhole bh) throws IOException {
+        // DIFF: hard-coded
+        // KeyValueMap_222927891 aggregation_state_map = new KeyValueMap_222927891();
+        // ArrowTableReader lineitem = cCtx.getArrowReader(0);
+        while (lineitem_table.loadNextBatch()) {
+            org.apache.arrow.vector.Float8Vector lineitem_vc_0 = ((org.apache.arrow.vector.Float8Vector) lineitem_table.getVector(4));
+            org.apache.arrow.vector.Float8Vector lineitem_vc_1 = ((org.apache.arrow.vector.Float8Vector) lineitem_table.getVector(5));
+            org.apache.arrow.vector.Float8Vector lineitem_vc_2 = ((org.apache.arrow.vector.Float8Vector) lineitem_table.getVector(6));
+            org.apache.arrow.vector.Float8Vector lineitem_vc_3 = ((org.apache.arrow.vector.Float8Vector) lineitem_table.getVector(7));
+            org.apache.arrow.vector.FixedSizeBinaryVector lineitem_vc_4 = ((org.apache.arrow.vector.FixedSizeBinaryVector) lineitem_table.getVector(8));
+            org.apache.arrow.vector.FixedSizeBinaryVector lineitem_vc_5 = ((org.apache.arrow.vector.FixedSizeBinaryVector) lineitem_table.getVector(9));
+            org.apache.arrow.vector.DateDayVector lineitem_vc_6 = ((org.apache.arrow.vector.DateDayVector) lineitem_table.getVector(10));
+            int recordCount = lineitem_vc_0.getValueCount();
+            for (int aviv = 0; aviv < recordCount; aviv++) {
+                int ordinal_value = lineitem_vc_6.get(aviv);
+                if (!((ordinal_value <= 10471))) {
+                    continue;
+                }
+                int projection_literal = 1;
+                double projection_computation_result = (projection_literal - lineitem_vc_2.get(aviv));
+                double projection_computation_result_0 = (lineitem_vc_1.get(aviv) * projection_computation_result);
+                int projection_literal_0 = 1;
+                double projection_computation_result_1 = (projection_literal_0 - lineitem_vc_2.get(aviv));
+                double projection_computation_result_2 = (lineitem_vc_1.get(aviv) * projection_computation_result_1);
+                int projection_literal_1 = 1;
+                double projection_computation_result_3 = (projection_literal_1 + lineitem_vc_3.get(aviv));
+                double projection_computation_result_4 = (projection_computation_result_2 * projection_computation_result_3);
+                byte[] ordinal_value_0 = lineitem_vc_4.get(aviv);
+                byte[] ordinal_value_1 = lineitem_vc_5.get(aviv);
+                long group_key_pre_hash = Char_Arr_Hash_Function.preHash(ordinal_value_0);
+                group_key_pre_hash ^= Char_Arr_Hash_Function.preHash(ordinal_value_1);
+                double ordinal_value_2 = lineitem_vc_0.get(aviv);
+                double ordinal_value_3 = lineitem_vc_1.get(aviv);
+                double ordinal_value_4 = lineitem_vc_2.get(aviv);
+                aggregation_state_map.incrementForKey(ordinal_value_0, ordinal_value_1, group_key_pre_hash, ordinal_value_2, ordinal_value_3, projection_computation_result_0, projection_computation_result_4, 1, ordinal_value_4);
+            }
+        }
+        for (int key_i = 0; key_i < aggregation_state_map.numberOfRecords; key_i++) {
+            byte[] groupKey_0 = aggregation_state_map.keys_ord_0[key_i];
+            byte[] groupKey_1 = aggregation_state_map.keys_ord_1[key_i];
+            double aggregation_0_value = aggregation_state_map.values_ord_0[key_i];
+            double aggregation_1_value = aggregation_state_map.values_ord_1[key_i];
+            double aggregation_2_value = aggregation_state_map.values_ord_2[key_i];
+            double aggregation_3_value = aggregation_state_map.values_ord_3[key_i];
+            int aggregation_4_value = aggregation_state_map.values_ord_4[key_i];
+            double aggregation_5_value = aggregation_state_map.values_ord_5[key_i];
+            double projection_computation_result = (aggregation_0_value / aggregation_4_value);
+            double projection_computation_result_0 = (aggregation_1_value / aggregation_4_value);
+            double projection_computation_result_1 = (aggregation_5_value / aggregation_4_value);
+            // DIFF: changed for result verification
+            // System.out.print(new java.lang.String(groupKey_0) + ", ");
+            // System.out.print(new java.lang.String(groupKey_1) + ", ");
+            // System.out.print(aggregation_0_value + ", ");
+            // System.out.print(aggregation_1_value + ", ");
+            // System.out.print(aggregation_2_value + ", ");
+            // System.out.print(aggregation_3_value + ", ");
+            // System.out.print(projection_computation_result + ", ");
+            // System.out.print(projection_computation_result_0 + ", ");
+            // System.out.print(projection_computation_result_1 + ", ");
+            // System.out.println(aggregation_4_value);
+            this.resultReturnFlag[key_i] = groupKey_0;
+            this.resultLineStatus[key_i] = groupKey_1;
+            this.resultSumQuantity[key_i] = aggregation_0_value;
+            this.resultSumBasePrice[key_i] = aggregation_1_value;
+            this.resultSumDiscPrice[key_i] = aggregation_2_value;
+            this.resultSumCharge[key_i] = aggregation_3_value;
+            this.resultAvgQuantity[key_i] = projection_computation_result;
+            this.resultAvgPrice[key_i] = projection_computation_result_0;
+            this.resultAvgDisc[key_i] = projection_computation_result_1;
+            this.resultCountOrder[key_i] = aggregation_4_value;
+        }
+
+        // DIFF: prevent optimising the result away
+        bh.consume(this.resultReturnFlag);
+        bh.consume(this.resultLineStatus);
+        bh.consume(this.resultSumQuantity);
+        bh.consume(this.resultSumBasePrice);
+        bh.consume(this.resultSumDiscPrice);
+        bh.consume(this.resultSumCharge);
+        bh.consume(this.resultAvgQuantity);
+        bh.consume(this.resultAvgPrice);
+        bh.consume(this.resultAvgDisc);
+        bh.consume(this.resultCountOrder);
+    }
+
+}
---
+++ src/main/java/benchmarks/tpch/q1_no_sort_hard_coded/ResultVerifier.java
@@ -0,0 +1,142 @@
+package benchmarks.tpch.q1_no_sort_hard_coded;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Class to verify the result of TPC-H Q1 query based on the dataset in an unordered fashion.
+ */
+public class ResultVerifier {
+
+    /**
+     * Contains the correct result;
+     */
+    private final byte[][] correctResultReturnFlag;
+    private final byte[][] correctResultLineStatus;
+    private final double[] correctResultSumQuantity;
+    private final double[] correctResultSumBasePrice;
+    private final double[] correctResultSumDiscPrice;
+    private final double[] correctResultSumCharge;
+    private final double[] correctResultAvgQuantity;
+    private final double[] correctResultAvgPrice;
+    private final double[] correctResultAvgDisc;
+    private final int[] correctResultCountOrder;
+
+    /**
+     * Creates a {@link ResultVerifier} instance for a specific dataset and query paradigm.
+     * @param resultFile The path to the CSV file containing the correct result.
+     */
+    public ResultVerifier(String resultFile) throws Exception {
+        // Fill the arrays based on the expected result csv for the dataset
+        Reader expectedResultCsvFileReader = new FileReader(resultFile);
+        BufferedReader expectedResultCsv = new BufferedReader(expectedResultCsvFileReader);
+
+        CSVFormat expectedResultCsvFormat = CSVFormat.DEFAULT.builder()
+                .setSkipHeaderRecord(true)
+                .setIgnoreSurroundingSpaces(true)
+                .build();
+
+        Iterable<CSVRecord> records = expectedResultCsvFormat.parse(expectedResultCsv);
+        List<CSVRecord> recordList = new ArrayList<>();
+        records.forEach(recordList::add);
+
+        // Initialise the result arrays
+        int resultSize = recordList.size();
+        this.correctResultReturnFlag = new byte[resultSize][];
+        this.correctResultLineStatus = new byte[resultSize][];
+        this.correctResultSumQuantity = new double[resultSize];
+        this.correctResultSumBasePrice = new double[resultSize];
+        this.correctResultSumDiscPrice = new double[resultSize];
+        this.correctResultSumCharge = new double[resultSize];
+        this.correctResultAvgQuantity = new double[resultSize];
+        this.correctResultAvgPrice = new double[resultSize];
+        this.correctResultAvgDisc = new double[resultSize];
+        this.correctResultCountOrder = new int[resultSize];
+
+        for (int i = 0; i < resultSize; i++) {
+            CSVRecord r = recordList.get(i);
+            this.correctResultReturnFlag[i] = r.get(0).getBytes(StandardCharsets.US_ASCII);
+            this.correctResultLineStatus[i] = r.get(1).getBytes(StandardCharsets.US_ASCII);
+            this.correctResultSumQuantity[i] = Double.parseDouble(r.get(2));
+            this.correctResultSumBasePrice[i] = Double.parseDouble(r.get(3));
+            this.correctResultSumDiscPrice[i] = Double.parseDouble(r.get(4));
+            this.correctResultSumCharge[i] = Double.parseDouble(r.get(5));
+            this.correctResultAvgQuantity[i] = Double.parseDouble(r.get(6));
+            this.correctResultAvgPrice[i] = Double.parseDouble(r.get(7));
+            this.correctResultAvgDisc[i] = Double.parseDouble(r.get(8));
+            this.correctResultCountOrder[i] = Integer.parseInt(r.get(9));
+        }
+
+        expectedResultCsv.close();
+        expectedResultCsvFileReader.close();
+    }
+
+    /**
+     * Method to check if the result computed in a query is indeed correct.
+     * @return {@code true} iff the result matches the expected result.
+     */
+    public boolean resultCorrect(
+            byte[][] resultReturnFlag,
+            byte[][] resultLineStatus,
+            double[] resultSumQuantity,
+            double[] resultSumBasePrice,
+            double[] resultSumDiscPrice,
+            double[] resultSumCharge,
+            double[] resultAvgQuantity,
+            double[] resultAvgPrice,
+            double[] resultAvgDisc,
+            int[] resultCountOrder)
+    {
+        if (resultReturnFlag.length != this.correctResultReturnFlag.length)
+            return false;
+
+        // Check for each of the group keys if its result is correctly present in the result
+        // Initially each element of the array is false, and will be marked true if it has been
+        // found in the result
+        boolean[] groupCorrectInResult = new boolean[this.correctResultReturnFlag.length];
+
+        for (int resultIndex = 0; resultIndex < resultReturnFlag.length; resultIndex++) {
+            // First find the group index in the correct result
+            int correctResultIndex = -1;
+            for (int pcri = 0; pcri < this.correctResultReturnFlag.length; pcri++) {
+                if (Arrays.equals(resultReturnFlag[resultIndex], this.correctResultReturnFlag[pcri])
+                        && Arrays.equals(resultLineStatus[resultIndex], this.correctResultLineStatus[pcri])) {
+                    correctResultIndex = pcri;
+                    break;
+                }
+            }
+
+            if (correctResultIndex == -1)
+                return false;
+
+            // Check the remaining columns
+            groupCorrectInResult[correctResultIndex] =
+                       resultSumQuantity[resultIndex] == correctResultSumQuantity[correctResultIndex]
+                    && (Math.abs(resultSumBasePrice[resultIndex] - correctResultSumBasePrice[correctResultIndex]) < 1e-2)
+                    && (Math.abs(resultSumDiscPrice[resultIndex] - correctResultSumDiscPrice[correctResultIndex]) < 1e-2)
+                    && (Math.abs(resultSumCharge[resultIndex] - correctResultSumCharge[correctResultIndex]) < 1e-2)
+                    && (Math.abs(resultAvgQuantity[resultIndex] - correctResultAvgQuantity[correctResultIndex]) < 1e-2)
+                    && (Math.abs(resultAvgPrice[resultIndex] - correctResultAvgPrice[correctResultIndex]) < 1e-2)
+                    && (Math.abs(resultAvgDisc[resultIndex] - correctResultAvgDisc[correctResultIndex])) < 1e-2
+                    && resultCountOrder[resultIndex] == correctResultCountOrder[correctResultIndex];
+
+        }
+
+        // Return whether all groups have been correctly found in the result
+        for (boolean currentGroupCorrect : groupCorrectInResult)
+            if (!currentGroupCorrect)
+                return false;
+
+        // All groups match
+        return true;
+    }
+
+}
